b374a1f9e4564b6f0f2f0a9aaec83783fd3975bd
Matthew Cillis <MattCillis@Matts-MacBook-Pro.local>
2020-02-19 17:58:39 -0500

    hw completed

--- pset-functions.js
+++ pset-functions.js
@@ -30,7 +30,24 @@ Use the following test cases to confirm your program meets the success criteria
 console.log('Problem 1:')
 
 // Add your code below this line
-
+// create function called blackJack w/ 2 parameters (playerCardScore, dealerCardScore)
+function blackJack(playerCardScore, dealerCardScore){
+  //if both players are over 21, return 0
+  if(playerCardScore > 21 && dealerCardScore > 21){
+    return 0
+    //if playerCardScore >= 21, && dealerCardScore >= 21 && playerCardScore > dealerCardScore, return higher score
+  } else if(playerCardScore <= 21 && dealerCardScore <= 21){
+    return Math.max(playerCardScore, dealerCardScore)
+  } else if(playerCardScore > 21 && dealerCardScore <= 21){
+    return dealerCardScore
+  } else{
+    return(playerCardScore)
+  }
+}
+console.log(blackJack(19,21))
+console.log(blackJack(22,22))
+console.log(blackJack(19,22))
+console.log(blackJack(21,19))
 // Add your code above this line
 
 /** added for formatting purposes **/
@@ -75,8 +92,37 @@ Use the following test cases to confirm your program meets the success criteria
 console.log('Problem 2:')
 
 // Add your code below this line
-
+//create function called scrabbleScore with word
+function scrabbleScore(word){
+//convert the word to uppercase and split it to an array
+  const letters = word.toUpperCase().split('')
+//create variable for result scores
+  let score = 0
+//loop through word w/ forEach
+  letters.forEach((letter) => {
+    if(['A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T'].includes(letter)){
+        score += 1
+    } else if(['D', 'G'].includes(letter)){
+        score += 2
+    } else if(['B', 'C', 'M', 'P'].includes(letter)){
+        score += 3
+    } else if(['F', 'H', 'V', 'W', 'Y'].includes(letter)){
+        score += 4
+    } else if(['K'].includes(letter)){
+        score += 5
+    } else if(['J', 'X'].includes(letter)){
+        score += 8
+    } else if(['Q', 'Z'].includes(letter)){
+        score += 10
+    }
+})
+    return score
+}
 // Add your code above this line
+
+console.log(scrabbleScore('function'))
+console.log(scrabbleScore("javascript"))
+console.log(scrabbleScore("cabbage"))
 
 /** added for formatting purposes **/
 console.log('')
@@ -113,8 +159,20 @@ Use the following test cases to confirm your program meets the success criteria
 ************/
 console.log('Problem 3:')
 
-// Add your code below this line
+  // Add your code below this line
+  //create a function called isPalindrome w/ a parameter called words
 
+  function isPalindrome(word){
+    return word === word.split('').reverse().join('')
+  }
+
+  //refactored using arrow function
+  // const isPalindrome = (word) => word === word.split('').reverse().join('')
+
+  console.log(isPalindrome('noon'))
+  console.log(isPalindrome('racecar'))
+  console.log(isPalindrome('moon'))
+  console.log(isPalindrome('run'))
 // Add your code above this line
 
 /** added for formatting purposes **/
@@ -147,6 +205,20 @@ Use the following test cases to confirm your program meets the success criteria
 console.log('Problem 4:')
 
 // Add your code below this line
+//create a function called doubleLetters with word parameter
+function doubleLetters(word){
+  for(i = 0; i <= word.length; i++) {
+        for(var j = i+1; j <= word.length; j++) {
+            if(word[j] == word[i]) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+console.log(doubleLetters("loop"))
+console.log(doubleLetters("rune"))
+console.log(doubleLetters("apple"))
 
 // Add your code above this line
 
@@ -207,7 +279,33 @@ Use the following test cases to confirm your program meets the success criteria
 console.log('Problem 5 - bonus:')
 
 // Add your code below this line
+// create a function called wordCount with parameter called phrase
+function wordCount(phrase) {
+  const words = phrase.split(' ')
+  const wordCount = {}
 
+  words.forEach((word) => {
+    // check if word exists as a key in wordCount object
+    // if it does not, then add word as key and value as 1
+    // if word already exists, then increment value by 1
+    if (wordCount[word] === undefined) {
+      // if letter does not already exist in wordCount object
+      wordCount[word] = 1
+    } else {
+      // letter already exists in wordCount object
+      // increment value by 1
+      wordCount[word] = wordCount[word] + 1
+    }
+    // console.log(wordCount)
+  })
+
+  return wordCount
+  }
+
+  //call function
+  console.log(wordCount("olly olly in come free"))
+  console.log(wordCount("Baby shark doo doo doo doo doo doo"))
+  console.log(wordCount("Humpty Dumpty sat on a wall Humpty Dumpty had a great fall"))
 // Add your code above this line
 
 /** added for formatting purposes **/
